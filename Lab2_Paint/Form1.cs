770d752b0c93632ba104ce449aa76d3b2db92422
romsv <rsviridov@sfedu.ru>
2021-09-29 03:16:44 +0300

    Draw line by Wu

--- Lab2_Paint/Form1.cs
+++ Lab2_Paint/Form1.cs
@@ -22,7 +22,7 @@ namespace Lab2_Paint
         public Form1()
         {
             InitializeComponent();
-            this.Work.Items.AddRange(new object[] { "Floodfill", "Flood fill ps", "Draw line", "Gradient", "Draw by mouse" });
+            this.Work.Items.AddRange(new object[] { "Floodfill", "Flood fill ps", "Draw line by Wu", "Gradient", "Draw by mouse", "Draw line by Bresenham" });
             picture = new Bitmap(Canvas.Width, Canvas.Height);
             for (int i = 0; i < Canvas.Width; ++i)
                 for (int j = 0; j < Canvas.Height; ++j)
@@ -145,6 +145,72 @@ namespace Lab2_Paint
                     };
                     break;
 
+                case 2:
+                    Canvas.MouseClick += null;
+                    bool drawSegment = false;
+                    int x_0 = 0, y_0 = 0, x_1, y_1;
+                    Graphics g = Canvas.CreateGraphics();
+
+                    void plot (int x, int y, double c)
+                    {
+                        int red = (int)(SelectedColor.R * c);
+                        int green = (int)(SelectedColor.G * c);
+                        int blue = (int)(SelectedColor.B * c);
+
+                        Color color = Color.FromArgb(red,green,blue);
+                        g.FillRectangle(new SolidBrush(color), x, y, 2, 2);
+                    }
+
+                    void drawPoint(int x0, int y0, int x1, int y1)
+                    {
+                        float dx = x1 - x0, dy = y1 - y0;
+                        float gradient = dy / dx;
+
+                        if (gradient <= 1)
+                        {
+                            float y = y0 + gradient;
+                            for (int x = x0 + 1; x <= x1 - 1; x++)
+                            {
+                                plot(x, (int)y, 1 - (y - (int)y));
+                                plot(x, (int)y + 1, y - (int)y);
+                                y += gradient;
+                            }
+                        }
+                        else
+                        {
+                            gradient = dx / dy;
+                            float x = x0 + gradient;
+                            for (int y = y0 + 1; y <= y1 - 1; y++)
+                            {
+                                plot((int)x, y, 1 - (x - (int)x));
+                                plot((int)x + 1, y, x - (int)x);
+                                x += gradient;
+                            }
+                        }
+                    }
+                    
+                    Canvas.MouseDown += (object send, MouseEventArgs ev) =>
+                    {      
+                        if (drawSegment)
+                        {
+                            x_1 = ev.X;
+                            y_1 = ev.Y;
+                            if (x_1 >= x_0)
+                                drawPoint(x_0, y_0, x_1, y_1);
+                            else
+                                drawPoint(x_1, y_1, x_0, y_0);
+                            x_0 = x_1;
+                            y_0 = y_1; 
+                        }         
+                        else
+                        {
+                            x_0 = ev.X;
+                            y_0 = ev.Y;
+                            drawSegment = true;
+                        }
+                    };
+                    break;
+
                 case 4:
 
                     Canvas.MouseClick += null;
@@ -168,7 +234,64 @@ namespace Lab2_Paint
                         Canvas.MouseMove += null; 
                         Canvas.Image = picture;
                     };
-                    break; 
+                    break;
+                case 5:
+                    Canvas.MouseClick += null;
+                    bool drawSegment_ = false;
+                    int x_0_ = 0, y_0_ = 0, x_1_, y_1_;
+                    g = Canvas.CreateGraphics();
+
+                    void plot_(int x, int y)
+                    {
+                        Color color = SelectedColor;
+                        g.FillRectangle(new SolidBrush(color), x, y, 2, 2);
+                    }
+
+                    void drawPoint_(int x0, int y0, int x1, int y1)
+                    {
+                        int deltax = Math.Abs(x1 - x0);
+                        int deltay = Math.Abs(y1 - y0);
+                        double error = 0;
+                        double deltaerr = (deltay + 1) / (deltax + 1);
+                        int y = y0;
+                        int diry = y1 - y0;
+                        if (diry > 0)
+                            diry = 1;
+                        if (diry < 0)
+                            diry = -1;
+                        for (int x = x0; x <= x1; x++)
+                        {
+                            plot_(x, y);
+                            error += deltaerr;
+                            if (error >= 1.0)
+                            {
+                                y += diry;
+                                error -= 1.0;
+                            }
+                        }
+                    }
+
+                    Canvas.MouseDown += (object send, MouseEventArgs ev) =>
+                    {
+                        if (drawSegment_)
+                        {
+                            x_1_ = ev.X;
+                            y_1_ = ev.Y;
+                            if (x_1_ >= x_0_)
+                                drawPoint_(x_0_, y_0_, x_1_, y_1_);
+                            else
+                                drawPoint_(x_1_, y_1_, x_0_, y_0_);
+                            x_0_ = x_1_;
+                            y_0_ = y_1_;
+                        }
+                        else
+                        {
+                            x_0_ = ev.X;
+                            y_0_ = ev.Y;
+                            drawSegment_ = true;
+                        }
+                    };
+                    break;
             }
         }
     }
